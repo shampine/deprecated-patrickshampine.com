eca010754c9bf093077b0702325d5d5af2a064e5
patrickshampine <patrick@patrickshampine.com>
2014-05-17 19:49:32 -0700

    Blog updates

--- public/wp-content/themes/shampine/single.php
+++ public/wp-content/themes/shampine/single.php
@@ -1,23 +1,41 @@
 <?php get_header(); ?>
 
 <section class="single post">
-  <div class="container">
-    <article class="row">
-      <div class="col-md-10 col-md-offset-1 title">
-      <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-      <h1><?php the_title(); ?></h1>
-      <p style="text-align: center;"><em><?php the_time('l, F jS, Y'); ?></em></p>
-      <?php the_content(); ?>
-          <div class="social">
-            <div class="g-plusone" data-size="medium"></div>
-            <div class="twitter"><a href="https://twitter.com/share" class="twitter-share-button" data-via="patrickshampine">Tweet</a></div>
-          </div>
-      <?php comments_template(); ?>
-      <?php endwhile; else: ?>
-      <p><?php _e('Sorry, this page does not exist.'); ?></p>
-      <?php endif; ?>
-      </div>
-    </article>
+  <div class="container"><?php 
+
+    if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+      <article class="row">
+        <div class="col-md-12 title"><?php
+        
+          $masthead = get_field('masthead');
+
+          if($masthead) {
+            echo '<div class="masthead"><img src="'.$masthead['url'].'" alt="'.$masthead['alt'].'" style="width:100%;height:auto;"></div>';
+          }
+
+          echo '<h1>'.get_the_title().'</h1>';
+
+          echo '<p class="aligncenter"><em>'.get_the_time('l, F jS, Y').'</em></p>';
+          
+          the_content(); 
+
+          // gallery? ?>
+
+      </article>
+
+      <section class="social">
+        <div class="g-plusone" data-size="medium"></div>
+        <div class="twitter"><a href="https://twitter.com/share" class="twitter-share-button" data-via="patrickshampine">Tweet</a></div>
+      </section>
+
+      <section class="comments">
+        <?php comments_template(); ?>
+      </section><?php
+
+    endwhile; else: ?>
+      <article><p><?php _e('Sorry, this page does not exist.'); ?></p></article><?php
+    endif; ?>
+
   </div>
 </section>
 
