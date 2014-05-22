0ba00f42bef1725664ffe906f08ac9343d668ed7
patrickshampine <patrick@patrickshampine.com>
2014-05-21 22:33:30 -0700

    Added non https script... les sigh

--- public/wp-content/themes/shampine/inc/assets.php
+++ public/wp-content/themes/shampine/inc/assets.php
@@ -4,7 +4,7 @@ function enqueue_scripts() {
 
   // Libraries
   wp_deregister_script('jquery');
-  wp_enqueue_script('jquery', 'http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js', false, null, true);
+  wp_enqueue_script('jquery', 'https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js', false, null, true);
   wp_enqueue_script('main', get_bloginfo('template_directory') . '/js/main.js', array('jquery'), null, false);
 
   if (is_page('contact')) { 
