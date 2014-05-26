ba328920e55c3f6cf5693246f67e0ed178d38487
patrickshampine <patrick@patrickshampine.com>
2014-05-26 15:53:07 -0700

    Added cache buster

--- public/wp-content/themes/shampine/header.php
+++ public/wp-content/themes/shampine/header.php
@@ -4,7 +4,9 @@
   <meta charset="utf-8">
   <title><?php wp_title('|'); ?></title>
   <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
-  <meta name="p:domain_verify" content="46378d3c32c3c64b9e51ecf0f9ed9155"/>
+  <meta name="p:domain_verify" content="46378d3c32c3c64b9e51ecf0f9ed9155"/><?php
+  $style_path = get_template_directory() . '/css/screen.css';
+  $style_uri  = get_bloginfo('template_url') . '/css/screen.css' . '?' . filemtime($style_path); ?>
   <link href="/wp-content/themes/shampine/css/screen.css" rel="stylesheet">
   <link href='https://fonts.googleapis.com/css?family=Open+Sans:300,400,600' rel='stylesheet' type='text/css'>
   <link rel="shortcut icon" href="/wp-content/themes/shampine/img/favicon.png" />
