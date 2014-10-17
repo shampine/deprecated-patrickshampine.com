043cf10030850dafb7d788d11a61b37e05c97eba
Patrick Shampine <patrick@patrickshampine.com>
2014-10-17 06:03:31 +0000

    Plugin updates

--- public/wp-content/plugins/crayon-syntax-highlighter/crayon_wp.class.php
+++ public/wp-content/plugins/crayon-syntax-highlighter/crayon_wp.class.php
@@ -1,9 +1,9 @@
 <?php
 /*
 Plugin Name: Crayon Syntax Highlighter
-Plugin URI: https://github.com/aramkocharyan/crayon-syntax-highlighter
+Plugin URI: https://github.com/aramk/crayon-syntax-highlighter
 Description: Supports multiple languages, themes, highlighting from a URL, local file or post text.
-Version: 2.6.7
+Version: 2.6.8
 Author: Aram Kocharyan
 Author URI: http://aramk.com/
 Text Domain: crayon-syntax-highlighter
