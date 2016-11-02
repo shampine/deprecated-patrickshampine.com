f8fe1cee66c9614e4a93b45d9161e61b36e50648
Patrick Shampine <patrick@patrickshampine.com>
2016-11-02 08:27:11 +0000

    Plugin updates

--- public/wp-content/plugins/wordpress-seo/vendor/autoload.php
+++ public/wp-content/plugins/wordpress-seo/vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInitf20029986052e53fa9826780b2029c06::getLoader();
+return ComposerAutoloaderInit6a5e17a20c03a769dd76c8f83b4da3e1::getLoader();
--- public/wp-content/plugins/wordpress-seo/vendor/autoload_52.php
+++ public/wp-content/plugins/wordpress-seo/vendor/autoload_52.php
@@ -4,4 +4,4 @@
 
 require_once dirname(__FILE__) . '/composer'.'/autoload_real_52.php';
 
-return ComposerAutoloaderInit0b36b471ac303c879fed046d17914a59::getLoader();
+return ComposerAutoloaderInit7fbfbb0d63cd7b0293e76e02c6a890f6::getLoader();
